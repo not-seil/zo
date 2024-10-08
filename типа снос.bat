79184a4586b2490f4474040708ec4f496e3a405f
not-seil <seilxx@vk.com>
2024-10-08 22:46:07 +0700

    21

---
+++ типа снос.bat
@@ -0,0 +1,180 @@
+@e
+title hsdhgdhsd.bat
+@echo off
+title FALCON SNOS / By EvilEnv and Seil
+color 3
+chcp 65001
+@echo    #########################################################
+@echo    #               
+@echo    #     F A L C O N   S O F T W A R E
+@echo    #                                                                  
+@echo    #     Launcher has been updated successfully!     
+@echo    #                                                                         
+@echo    #     Please, restart this application one time more.          
+@echo    #                                                                         
+@echo    #                                                                         
+@echo    #                By EvilEnv and Seil                         
+@echo    #                By EvilEnv and Seil                                              
+@echo    #                By EvilEnv and Seil                                              
+@echo    #                                                                         
+@echo    #                                                                         
+@echo    #                                                                         
+@echo    #########################################################              
+set /p "yes=Нажмите Enter для продолжения"
+cls
+
+color 4
+title FaLcOn SnOs
+@echo    #########################################################
+@echo    #               
+@echo    #     F A L C O N   S O F T W A R E
+@echo    #          
+@echo    #########################################################                                
+echo ----------Введите номер / тег / айди
+echo ----------Введите номер / тег / айди
+echo ----------Введите номер / тег / айди
+set /p "input=------>"
+cls
+
+title fAlCoN sNoS
+@echo    #########################################################
+@echo    #               
+@echo    #     F A L C O N   S O F T W A R E
+@echo    #          
+@echo    #########################################################           
+echo Введите количество потоков (Есле не занаете что это, пишите от 100 до 500)
+echo Введите количество потоков (Есле не занаете что это, пишите от 100 до 500)
+echo Введите количество потоков (Есле не занаете что это, пишите от 100 до 500)
+set /p "service=------>"
+cls
+
+title FALCON SNOS / Status: Поиск отправителей...
+echo Аккаунтов для жалоб найдено: 54
+PING localhost -n 2 >NUL
+echo Отправка запросов, ожидайте...
+title FALCON SNOS / Status: Отправка репортов...
+PING localhost -n 2 >NUL
+echo Отправлено запросов: 1
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 2
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 3
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 4
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 5
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 6
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 7
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 8
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 9
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 10
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 11
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 12
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 13
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 14
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 15
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 16
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 17
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 18
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 19
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 20
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 21
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 22
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 23
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 24
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 25
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 26
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 27
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 28
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 29
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 30
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 31
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 32
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 33
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 34
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 35
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 36
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 37
+PING localhost -n 2 >NUL
+echo Отправлено запросов: 38
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 39
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 40
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 41
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 42
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 43
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 44
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 45
+PING localhost -n 2 >NUL
+echo Отправлено запросов: 46
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 47
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 48
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 49
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 50
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 51
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 52
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 53
+PING localhost -n 1 >NUL
+echo Отправлено запросов: 54
+PING localhost -n 4 >NUL
+title FALCON SNOS / Status: Отправлено!
+echo Запросы были успешно отправлены, ожидайте!
+PING localhost -n 2 >NUL
+echo #
+echo #
+echo #
+set /p "yes=Нажмите Enter для продолжения"
+cls
+@echo    #########################################################
+@echo    #               
+@echo    #     F A L C O N   S O F T W A R E
+@echo    #          
+@echo    #########################################################           
+echo Спасибо за использование!
+pause
+exit
